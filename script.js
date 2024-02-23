b9853f6d679c140bcdb5932bd82dfb19d540162b
Dhruv Patel <122099280+dhruvurgr8@users.noreply.github.com>
2024-02-23 11:08:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,31 @@
+const btn = document.querySelector('.btn');
+const table = document.querySelector('#sampleTable');
+let rowNum = 3;
+
 function insert_Row() {
-    //Write your code here
-  
-  
+    btn.addEventListener('click', (e) => {
+        let colNum = 1;
+
+        const row = document.createElement('tr');
+
+        
+        const col1 = document.createElement('td');
+        col1.textContent = `Row${rowNum} cell${colNum}`;
+        row.appendChild(col1);
+        colNum++;
+
+        
+        const col2 = document.createElement('td');
+        col2.textContent = `Row${rowNum} cell${colNum}`;
+        row.appendChild(col2);
+
+        
+        rowNum++;
+
+        
+        table.appendChild(row);
+    });
 }
+
+// Call the function to set up the event listener
+insert_Row();
